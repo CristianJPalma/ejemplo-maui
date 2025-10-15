bb59ced2bd5612d5d9ae3142e76372f6778dba3c
CristianJPalma <cristianspalmas@gmail.com>
2025-10-14 22:14:27 -0500

    Mejora Colores

--- MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+069edf09ba55c455349950e6d9f29e197c7f3ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+899d2daacc70d60f6e68b440d517f6a254a5ae7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
