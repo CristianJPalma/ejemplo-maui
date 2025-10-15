3fc001f75337a9529ef69dd65e5628f5546401c2
CristianJPalma <cristianspalmas@gmail.com>
2025-10-15 18:19:39 -0500

    Metodo delete

--- MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f6573c40abd2640a84be21f505a7654f78537ca4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4a18bbd6ec81d34e869a4530052f7b79a8721efe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
