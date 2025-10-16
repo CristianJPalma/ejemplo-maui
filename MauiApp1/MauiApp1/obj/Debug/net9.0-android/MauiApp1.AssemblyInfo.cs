2e1304e6d477b00c44f26c79619d06f1dec85152
CristianJPalma <cristianspalmas@gmail.com>
2025-10-16 15:14:20 -0500

    Boton delete

--- MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+12372b402a760db4c89e4e6a06991bf31bac4608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a9d9d17fb70e9515bff30f0a27fc9eae9508a619")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
